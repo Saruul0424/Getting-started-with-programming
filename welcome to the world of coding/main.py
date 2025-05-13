7391de9f4cb8f982d04fb18540894c7eab867431
Saruul <Darkhanbatsaruul@gmail.com>
2025-05-13 21:05:23 +0800

    commit

---
+++ welcome to the world of coding/main.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print(welcome to the world of programming)
+
+#Print a number
+print(4)
+
+# Next line after print
+print("Hello phyton \n")
+
+#Pass multiplte arguments for printing
+print("hello" ,5)
+
+# End argument of print statement
+print("welcome to", end="*")
